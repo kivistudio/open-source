50755fec9a27d948e308c861b2c4cdf40fca5cde
Ela <elzbieta.siwy@gmail.com>
2023-02-28 15:33:17 +0000

    Release 1.0.0

---
+++ dist/bundle.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";let e=document.getElementById("size-toggle");e.addEventListener("change",(function(){var t;t=e.checked,document.body.style["font-size"]=t?"2em":"1em"}))})();
+//# sourceMappingURL=bundle.js.map
